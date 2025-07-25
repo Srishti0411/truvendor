7e66d13b4085e4e11bb5d5027d9a87057e63f85c
Srishti0411 <srishti41106@gmail.com>
2025-07-26 01:05:24 +0530

    Update firebase.js

---
+++ src/firebase/firebase.js
@@ -0,0 +1,23 @@
+// src/firebase/firebase.js
+
+// Import Firebase services
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// Your actual Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyDjVXp-GTpdoGVhOb4vhaR3BNSSjrkUTSY",
+  authDomain: "trustify-feb1b.firebaseapp.com",
+  projectId: "trustify-feb1b",
+  storageBucket: "trustify-feb1b.appspot.com",
+  messagingSenderId: "1044274572349",
+  appId: "1:1044274572349:web:f058ca6f361a002d458e54"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Export Auth and Firestore
+export const auth = getAuth(app);
+export const db = getFirestore(app);
