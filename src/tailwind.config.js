9215be0b050e535372eeac3185d2ca027b3e9508
Srishti0411 <srishti41106@gmail.com>
2025-07-28 00:48:11 +0530

    more updates

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-  "./public/index.html",
-  "./src/**/*.{js,jsx,ts,tsx}"
-],
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
   theme: {
     extend: {},
   },
